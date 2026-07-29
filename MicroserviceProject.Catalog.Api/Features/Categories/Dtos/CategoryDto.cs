2beb5ac02b11f9c0c163ce5baccb5c6b44aaeb6a
haktanbasak <haktanbasak9@gmail.com>
2026-07-29 04:04:56 +0300

    added category list endpoint

---
+++ MicroserviceProject.Catalog.Api/Features/Categories/Dtos/CategoryDto.cs
@@ -0,0 +1,4 @@
+﻿namespace MicroserviceProject.Catalog.Api.Features.Categories.Dtos
+{
+    public record class CategoryDto(Guid Id, string Name);
+}
---
+++ MicroserviceProject.Catalog.Api/Features/Categories/GetAll/GetAllCategoryEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MicroserviceProject.Catalog.Api.Features.Categories.Dtos;
+using MicroserviceProject.Catalog.Api.Repositories;
+using MicroserviceProject.Shared;
+using MicroserviceProject.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroserviceProject.Catalog.Api.Features.Categories.GetAll
+{
+    public class GetAllCategoryQuery: IRequest<ServiceResult<List<CategoryDto>>>;
+
+    public class GetAllCategoryQueryHandler(AppDbContext context) : IRequestHandler<GetAllCategoryQuery, ServiceResult<List<CategoryDto>>>
+    {
+        public async Task<ServiceResult<List<CategoryDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await context.Categories.ToListAsync();
+            var categoriesAsDto = categories.Select(c => new CategoryDto(c.Id, c.Name)).ToList();
+            return ServiceResult<List<CategoryDto>>.SuccessAsOk(categoriesAsDto);
+        }
+    }
+
+    public static class GetAllCategoryEndpoint
+    {
+        public static RouteGroupBuilder GetAllCategoryGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("/",
+                async (IMediator mediator) =>
+                    (await mediator.Send(new GetAllCategoryQuery())).ToGenericResult());
+
+            return group;
+        }
+    }
+}
